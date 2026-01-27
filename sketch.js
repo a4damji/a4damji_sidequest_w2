96c613d8c0a2595f72fc9fe0161d49b8f716d4a9
Amara Damji <a4damji@uwaterloo.ca>
2026-01-27 17:19:31 -0500

    more steps

--- sketch.js
+++ sketch.js
@@ -60,7 +60,8 @@ function setup() {
     { x: 0, y: floorY3, w: width, h: height - floorY3 }, // floor
     { x: 120, y: floorY3 - 70, w: 120, h: 12 }, // low step
     { x: 300, y: floorY3 - 120, w: 90, h: 12 }, // mid step
-    { x: 440, y: floorY3 - 180, w: 130, h: 12 }, // high step
+    { x: 440, y: floorY3 - 175, w: 130, h: 12 }, // high step
+    { x: 125, y: floorY3 - 175, w: 130, h: 12 }, //new step
     { x: 520, y: floorY3 - 70, w: 90, h: 12 }, // return ramp
   ];
 
